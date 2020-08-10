7f0272a71b1bf21df0f66b01e9e3ea6af30a767a
BastianIgnacio <bastian.sepulvedagarcia@gmail.com>
2020-08-09 20:56:15 -0400

    Inicio Proyecto con JWT en el back

---
+++ ProyectoPizzeria/back/puntoVenta/app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login() {
+        $credentials = request()->validate(['email' => 'required|email', 'password' => 'required|string|max:25']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error'=>'Invalid email or password'],401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    private function respondWithToken($token) {
+        return response()->json([
+            'token' => $token,
+            'access_type' => 'bearear',
+            'expires_in' => auth()->factory()->getTTL()*60
+        ]);
+    }
+
+    public function logout() {
+        auth()->logout();
+        return response()->json([
+            'msg' => 'Usuario deslogeado con exito'
+        ]);
+    }
+
+    public function refresh() {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    public function me() {
+        return response()->json(auth()->user());
+    }
+
+
+
+}
